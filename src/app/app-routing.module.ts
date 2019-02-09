85d37faba3e925ee50caf35a847b7a79675f0ffc
Mikolaj Pecak <mikolajpecak@gmail.com>
2019-02-09 22:44:54 +0100

    Create Week Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WeekComponent } from './week/week.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WeekComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/week/week.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-week',
+  templateUrl: './week.component.html',
+  styleUrls: ['./week.component.scss']
+})
+export class WeekComponent implements OnInit {
+  buttonDisabled = false;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  disableButton() {
+    this.buttonDisabled = true;
+  }
+}
